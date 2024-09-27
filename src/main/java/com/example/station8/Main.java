e324b7564bf3852ac05848703a5151d441c64df7
no215tyler <no.215.tyler@gmail.com>
2024-09-27 17:21:04 +0900

    station8: 条件分岐を扱えるようになろう

--- src/main/java/com/example/station8/Main.java
+++ src/main/java/com/example/station8/Main.java
@@ -4,12 +4,34 @@ public class Main {
     public static void question1(int subtotal) {
         // Station 8 問題(1)
         // ここから
+        int total = subtotal;
+        if (subtotal < 10000) {
+          System.out.println("送料800円を追加します");
+          total += 800;
+        }
+        System.out.println(total);
         // ここまで
     }
 
     public static void question2(int categoryId) {
         // Station 8 問題(2)
         // ここから
+        switch (categoryId) {
+          case 1:
+            System.out.println("食品");
+            break;
+          case 2:
+            System.out.println("家電");
+            break;
+          case 3:
+            System.out.println("家具");
+            break;
+          case 99:
+            System.out.println("その他");
+            break;
+          default:
+        }
+
         // ここまで
     }
 
