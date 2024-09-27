44acf35bf187c8527402e1791c4e4f6352c4ab7e
no215tyler <no.215.tyler@gmail.com>
2024-09-27 16:21:26 +0900

    station2: 型の違いを知ろう

--- src/main/java/com/example/station2/Main.java
+++ src/main/java/com/example/station2/Main.java
@@ -3,6 +3,12 @@ package com.example.station2;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        boolean bool = false;
+        int num = 42;
+        double pi = 3.141592;
+        System.out.println(bool);
+        System.out.println(num);
+        System.out.println(pi);
         // ここまで
     }
 }
