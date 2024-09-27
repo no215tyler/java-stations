5acb37c145c3a6f788331d16aee9c0e066887736
no215tyler <no.215.tyler@gmail.com>
2024-09-27 16:47:28 +0900

    station4: 基本的な演算子を扱えるようになろう

--- src/main/java/com/example/station4/Main.java
+++ src/main/java/com/example/station4/Main.java
@@ -3,6 +3,13 @@ package com.example.station4;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        int calcA = 18 * 28;
+        int calcB = 24 * 21;
+        System.out.println(calcA == calcB);
+
+        double pi = 3.141592;
+        double calcC = 21.9 / 7;
+        System.out.println(pi < calcC);
         // ここまで
     }
 }
