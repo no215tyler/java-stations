ab2bca4dc8c371af88f5f35421f815fa99fc41ac
no215tyler <no.215.tyler@gmail.com>
2024-09-27 17:09:51 +0900

    station7: 繰り返し処理を扱えるようになろう

--- src/main/java/com/example/station7/Main.java
+++ src/main/java/com/example/station7/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int[] itemPrices = { 1200, 2300, 3500, 2500, 1000, 2980, 9800, 4600, 5000, 2000 };
         // ここから
+        int sumPrices = 0;
+        for (int i = 0; i < itemPrices.length; i++) {
+          sumPrices += itemPrices[i];
+        }
+        System.out.println(sumPrices);
         // ここまで
     }
 }
