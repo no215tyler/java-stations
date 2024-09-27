d26e8e76aed1941ed68374a7c86c06d040f5b119
no215tyler <no.215.tyler@gmail.com>
2024-09-27 16:41:21 +0900

    station3: 文字列を扱えるようになろう

--- src/main/java/com/example/station3/Main.java
+++ src/main/java/com/example/station3/Main.java
@@ -3,6 +3,9 @@ package com.example.station3;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        System.out.println("/^^^^^^^^^^^\\\n" +
+                          "|\"TechTrain\"|\n" +
+                          "\\___________/");
         // ここまで
     }
 }
