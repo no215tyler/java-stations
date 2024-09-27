f70c9ee57048300142504d902b8e9794427391b0
no215tyler <no.215.tyler@gmail.com>
2024-09-27 17:04:18 +0900

    station6: 配列を扱えるようになろう

--- src/main/java/com/example/station6/Main.java
+++ src/main/java/com/example/station6/Main.java
@@ -1,8 +1,18 @@
 package com.example.station6;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         // ここから
+        int[] users = new int[5];
+        users[0] = 23;
+        users[1] = 36;
+        users[2] = 31;
+        users[3] = 48;
+        users[4] = 58;
+
+        double all = Arrays.stream(users).sum();
+        System.out.println(all / 5);
         // ここまで
     }
 }
